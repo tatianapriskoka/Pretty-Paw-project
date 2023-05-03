e1d0a42e6599c8b4325e10203bd0de50f641d94d
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-05-03 18:51:15 +0300

    feat: header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './index.html';
-
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 // new modules
 
 import './index.scss';
 
 //use modules
+
+const videeoBG = document.querySelector('.video-bg');
+videeoBG.innerHTML = `
+<source src = "video/video.webm" type= "video/webm"">
+<source src = "video/video.mp4" type= "video/mp4"">
+`;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -31,9 +31,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
-    // new CopyPlugin({
-    //   patterns: [{ from: 'static', to: './' }],
-    // }),
+    new CopyPlugin({
+      patterns: [{ from: 'static', to: './' }],
+    }),
   ],
   module: {
     rules: [
@@ -78,28 +78,28 @@ module.exports = {
         use: devMode
           ? []
           : [
-              {
-                loader: 'image-webpack-loader',
-                options: {
-                  mozjpeg: {
-                    progressive: true,
-                  },
-                  optipng: {
-                    enabled: false,
-                  },
-                  pngquant: {
-                    quality: [0.65, 0.9],
-                    speed: 4,
-                  },
-                  gifsicle: {
-                    interlaced: false,
-                  },
-                  webp: {
-                    quality: 75,
-                  },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.9],
+                  speed: 4,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                webp: {
+                  quality: 75,
                 },
               },
-            ],
+            },
+          ],
         type: 'asset/resource',
       },
       {
