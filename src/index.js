7f60dc2e4e92b003a86842efbc3ca9738979609b
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-05-05 20:06:41 +0300

    feat: section about slider

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import 'swiper/scss/pagination';
 // new modules
 
 import './index.scss';
+import { slidersInit } from './modules/slider';
 
 //use modules
+
+slidersInit('.about__slider', {
+    pagination: {
+        el: '.about__slider-pagination',
+        //bulletClass: 'about__bullet',
+        //bulletActiveClass: 'about__bullet_active',
+    }
+});
 
 const videeoBG = document.querySelector('.video-bg');
 videeoBG.innerHTML = `
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,5 +1,21 @@
 import { Swiper, Pagination, Autoplay } from "swiper";
+const params = {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    sliderPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    modules: [Autoplay, Pagination],
+}
 
-export const slidersInit = () => {
-
+export const slidersInit = (selectorSlider, newParams) => {
+    new Swiper(selectorSlider, {
+        ...params,
+        ...newParams,
+    })
 }
