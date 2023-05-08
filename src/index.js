a441c7466fd4a42dd061958c101c2e81b9ec7f86
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-05-08 18:42:26 +0300

    feat: section career

--- src/index.js
+++ src/index.js
@@ -16,6 +16,30 @@ slidersInit('.about__slider', {
     }
 });
 
+slidersInit('.career__slider', {
+    pagination: {
+        el: '.career__slider-pagination',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 'auto',
+            spaceBetween: 20,
+            pagination: false,
+        },
+
+        1024: {
+            slidesPerView: 'auto',
+            spaceBetween: 26,
+            pagination: false,
+        },
+        1240: {
+            slidesPerView: 'auto',
+            spaceBetween: 30,
+            pagination: false,
+        }
+    }
+});
+
 const videeoBG = document.querySelector('.video-bg');
 videeoBG.innerHTML = `
 <source src = "video/video.webm" type= "video/webm"">
