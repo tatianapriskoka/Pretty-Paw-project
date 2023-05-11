7edb21f30a0b04f8b92ecb55f732d8573bf22eb8
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-05-11 11:21:38 +0300

    fix: fix slider

--- src/index.js
+++ src/index.js
@@ -18,15 +18,21 @@ slidersInit('.about__slider', {
     }
 });
 
+const careerImageItems = document.querySelectorAll('.career__image-item');
+
+careerImageItems.forEach((item, i) =>
+    item.classList.add(`career__image-item_${i % 2 ? 'even' : 'odd'}`)
+);
+
 slidersInit('.career__slider', {
     pagination: {
         el: '.career__slider-pagination',
     },
     breakpoints: {
-        768: {
+        576: {
             slidesPerView: 'auto',
             spaceBetween: 20,
-            pagination: false,
+            pagination: true,
         },
 
         1024: {
